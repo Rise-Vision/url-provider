6702ccfe3d8a348409ac15859042853efeb40bef
Stuart Lees <stuartlees@gmail.com>
2017-10-05 17:34:39 -0400

    Use separate bash scripts for deploying stage and prod

---
+++ deploy-prod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster messaging-service-prod
+sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+sudo /opt/google-cloud-sdk/bin/gcloud config set container/use_client_certificate True
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials messaging-service-prod
+
+TAG=$(grep version package.json |grep -o '[0-9.]*');
+echo "Tag value is: $TAG"
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+kubectl patch deployment url-provider -p '{"spec":{"template":{"spec":{"containers":[{"name":"url-provider","image":'"$DOCKER_IMAGE_NAME"'":"v'"$TAG"'"}]}}}}'
---
+++ deploy-stage.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet config set container/cluster messaging-service-stage
+sudo /opt/google-cloud-sdk/bin/gcloud config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+sudo /opt/google-cloud-sdk/bin/gcloud config set container/use_client_certificate True
+sudo /opt/google-cloud-sdk/bin/gcloud --quiet container clusters get-credentials messaging-service-stage
+
+echo "Pushing image $DOCKER_IMAGE_NAME to registry..."
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push ${DOCKER_IMAGE_NAME}
+
+TAG=$(grep version package.json |grep -o '[0-9.]*');
+echo "Tag value is: $TAG"
+
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+kubectl patch deployment url-provider -p '{"spec":{"template":{"spec":{"containers":[{"name":"url-provider","image":'"$DOCKER_IMAGE_NAME"'":"v'"$TAG"'"}]}}}}'
