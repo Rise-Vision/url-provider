1627f03ca2ae73e13822501b40735e987d4fac12
Tyler Johnson <mail@tyler-johnson.ca>
2017-11-21 20:06:25 -0500

    Reorganize into higher-abstraction-at-top style

--- src/provider.js
+++ src/provider.js
@@ -4,15 +4,29 @@ const keyBytes = 32;
 const keyEnv = process.env.MSTOKEN_KEY || "0".repeat(keyBytes);
 const {CLIENT_ERROR, OK, AUTH_ERROR, SERVER_ERROR} = require("./status-codes.js");
 
-const validateBody = (body) => {
+module.exports = {
+  handleRequest(req, res) {
+    console.log(`Request Received ${JSON.stringify(req.body)}`);
+    validateBody(req.body)
+    .then(verifyMSToken)
+    .then(getSignedURL)
+    .then(url=>res.status(OK).send(url[0]))
+    .catch(error=>{
+      console.log("Error when providing an URL", error);
+      res.status(error.code || SERVER_ERROR).send(error.msg);
+    });
+  }
+}
+
+function validateBody(body) {
   if (!body || !body.data.timestamp || !body.data.filePath || !body.data.displayId || !body.hash) {
     return Promise.reject({code: CLIENT_ERROR, msg: "Invalid input"});
   }
 
   return Promise.resolve(body);
-};
+}
 
-const verifyMSToken = (body) => {
+function verifyMSToken(body) {
   return msTokenHandler.verify(body.data, body.hash, keyEnv)
     ? Promise.resolve(body)
     : Promise.reject({
@@ -21,22 +35,6 @@ const verifyMSToken = (body) => {
     });
 }
 
-const getSignedURL = (body) => {
+function getSignedURL(body) {
   return urlSigner.sign(body);
-}
-
-const handleRequest = (req, res) => {
-  console.log(`Request Received ${JSON.stringify(req.body)}`);
-  validateBody(req.body)
-  .then(verifyMSToken)
-  .then(getSignedURL)
-  .then(url=>res.status(OK).send(url[0]))
-  .catch(error=>{
-    console.log("Error when providing an URL", error);
-    res.status(error.code || SERVER_ERROR).send(error.msg);
-  });
-}
-
-module.exports = {
-  handleRequest
 }
