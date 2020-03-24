f9cf055b2bc8a7b5c8e7f8460520d01e55736b67
Tyler Johnson <mail@tyler-johnson.ca>
2020-03-24 13:12:50 -0400

    Update deps for vulns

--- src/url-signer.js
+++ src/url-signer.js
@@ -1,6 +1,6 @@
-const storage = require("@google-cloud/storage")({
-  projectId: "avid-life-623"
-});
+const {Storage} = require("@google-cloud/storage");
+const storage = new Storage();
+
 const threeHoursMillis = 3 * 60 * 60 * 1000; // eslint-disable-line no-magic-numbers
 
 module.exports = {
